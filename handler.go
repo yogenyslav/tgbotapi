359f7165f3ff9bf9949fb160b9c039ed27113dfd
Evgeny Gurov <evgenygurov9@mail.ru>
2023-12-08 19:08:16 +0300

    Added CallbackQuery, properly check for handler matching

--- handler.go
+++ handler.go
@@ -9,7 +9,25 @@ type HandlerFunc = func(*Ctx) error
 
 func (h *handler) processUpdate(c *Ctx) error {
 	if c.Update().Type == h.filter.Type {
-		return h.executable(c)
+		matched := false
+
+		switch c.Update().Type {
+		case CommandUpdateType:
+			matched = c.Message().Command() == h.filter.Value
+		case MessageUpdateType:
+			if h.filter.Value == "" {
+				matched = true
+			} else {
+				matched = c.Message().Text == h.filter.Value
+			}
+		case CallbackUpdateType:
+			matched = c.Update().CallbackQuery.Data == h.filter.Value
+		}
+		if matched {
+			return h.executable(c)
+		} else {
+			return InvalidType{c.Update().Message.Command(), h.filter.Value}
+		}
 	} else {
 		return HandlerMismatch{c.Update().Type, h.filter.Type}
 	}
--- query.go
+++ query.go
@@ -1,6 +1,13 @@
 package tgbotapi
 
 type CallbackQuery struct {
+	Id              string   `json:"id"`
+	From            *User    `json:"from"`
+	Message         *Message `json:"message,omitempty"`
+	InlineMessageId string   `json:"inline_message_id,omitempty"`
+	ChatInstance    string   `json:"chat_instance"`
+	Data            string   `json:"data,omitempty"`
+	GameShortName   string   `json:"game_short_name,omitempty"`
 }
 
 type InlineQuery struct {
